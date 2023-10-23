f34d8641b2c42af4cc0297a13532a3732d99e8d7
Alilymae <ortizaliah88@gmail.com>
2023-10-23 14:10:56 +0400

    Exercise-3.4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise-3.4.py
@@ -0,0 +1,7 @@
+#Invitation
+names = ["Itadori", "Megumi", "Nobara", "Gojo", "Nanami"]
+print (f"Hello! Join me at Jujutsu Restaurant {names[0]} on Monday")
+print (f"Hello! Join me at Jujutsu Restaurant {names[1]} on Monday")
+print (f"Hello! Join me at Jujutsu Restaurant {names[2]} on Monday")
+print (f"Hello! Join me at Jujutsu Restaurant {names[3]} on Monday")
+print (f"Hello! Join me at Jujutsu Restaurant {names[4]} on Monday")
