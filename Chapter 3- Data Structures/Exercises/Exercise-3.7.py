30957df877fa2fe238466004f12b04c7862b2c32
Alilymae <ortizaliah88@gmail.com>
2023-10-23 14:12:50 +0400

    Exercise-3.7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise-3.7.py
@@ -0,0 +1,31 @@
+#Places in the world, list in og order
+place = ["Seoul", "Kyoto", "Guangzhou", "Neuschwantein", "Athens"]
+print (place)
+
+#Sorting the list alphabetically
+print (sorted(place))
+
+#List in original order
+print (place) 
+
+#reverse list
+print (sorted(place, reverse = True)) 
+
+#List in og order
+print (place)
+
+#reverse order
+place.reverse()
+print (place)
+
+#reverse order but back to og
+place.reverse()
+print (place)
+
+#Sort alphabetically
+place.sort()
+print (place)
+
+#Sort but in reverse
+place.sort(reverse = True)
+print (place)
