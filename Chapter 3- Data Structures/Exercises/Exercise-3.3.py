8b8b2af9dfe955f7a116b5f7885afeb2d5e707b2
Alilymae <ortizaliah88@gmail.com>
2023-10-23 14:10:35 +0400

    Exercise-3.3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise-3.3.py
@@ -0,0 +1,7 @@
+#a voom vroom
+moto = ["Honda CBR300R", "Kawasaki Ninja 400", "Yamaha R3"]
+print (moto)
+for vroom in moto:
+    x = (f"YAY! We got a {vroom} motorcycle")
+    print (x)
+print ("Thank you for your service")
