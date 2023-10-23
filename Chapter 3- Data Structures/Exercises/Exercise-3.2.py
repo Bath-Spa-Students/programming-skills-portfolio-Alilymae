eca28d5169947e7b7b6aaae6000355c8f95b4cb9
Alilymae <ortizaliah88@gmail.com>
2023-10-23 14:10:18 +0400

    Exercise-3.2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise-3.2.py
@@ -0,0 +1,8 @@
+#Greetings
+names = ["Jr", "Kenji", "Angel", "Eriel", "Wyxy", "JM"]
+print ("Hi!, Welcome to planet earth!",names [0], "How may I help you today?")
+print ("Hi!, Welcome to planet earth!",names [1], "How may I help you today?")
+print ("Hi!, Welcome to planet earth!",names [2], "How may I help you today?")
+print ("Hi!, Welcome to planet earth!",names [3], "How may I help you today?")
+print ("Hi!, Welcome to planet earth!",names [4], "How may I help you today?")
+print ("Hi!, Welcome to planet earth!",names [5], "How may I help you today?")
