2c5c27fe7645b6365d9866b00ac60744551165fe
Alilymae <144819148+Alilymae@users.noreply.github.com>
2023-10-23 01:38:07 -0700

    Delete Chapter 1- Getting Started with Python/Exercises/Exercise 4.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise 4.py
+++
@@ -1,8 +0,0 @@
-a = "I may be stressed"
-b = " and depressed"
-c = " but I am blessed"
-print (a + b + c)
-a = "Being delulu"
-b = "is the solulu"
-c = "so may all your dreams come trululu"
-print ("-".join([a, b, c]))
