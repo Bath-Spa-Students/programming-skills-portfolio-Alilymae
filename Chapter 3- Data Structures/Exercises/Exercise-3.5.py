a5036d76b3ba6a5ef047c5a7eb45db99e3bf6c3a
Alilymae <ortizaliah88@gmail.com>
2023-10-23 14:12:26 +0400

    Exercise-3.5.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise-3.5.py
@@ -0,0 +1,14 @@
+#Invitation
+names = ["Itadori", "Megumi", "Nobara", "Gojo", "Nanami"]
+print (f"Hello! Welcome to Jujutsu Restaurant {names[0]} How may I help you?")
+print (f"Hello! Welcome to Jujutsu Restaurant {names[1]} How may I help you?")
+print (f"Hello! Welcome to Jujutsu Restaurant {names[2]} How may I help you?")
+print (f"Hello! Welcome to Jujutsu Restaurant {names[3]} How may I help you?")
+print (f"Hello! Welcome to Jujutsu Restaurant {names[4]} How may I help you?")
+print (f"It seems {names[4]} is busy so we'll give his seat to our new friend")
+names [4] = "Geto"
+print (f"Hello! Welcome to Jujutsu Restaurant {names[0]} How may I help you?")
+print (f"Hello! Welcome to Jujutsu Restaurant {names[1]} How may I help you?")
+print (f"Hello! Welcome to Jujutsu Restaurant {names[2]} How may I help you?")
+print (f"Hello! Welcome to Jujutsu Restaurant {names[3]} How may I help you?")
+print (f"Hello! Welcome to Jujutsu Restaurant {names[4]} How may I help you?")
