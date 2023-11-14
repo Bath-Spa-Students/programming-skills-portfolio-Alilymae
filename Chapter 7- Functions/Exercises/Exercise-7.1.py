651f993283e499743dfa0817f39a81d744a36fec
Alilymae <ortizaliah88@gmail.com>
2023-11-14 19:32:22 +0400

    Chapter 7 Ex 1

---
+++ Chapter 7- Functions/Exercises/Exercise-7.1.py
@@ -0,0 +1,8 @@
+#creating a function to talk abt function :)
+def d_message():
+    #definition of function as the statement
+    print ("A function is a block of code that runs only when its called.")
+
+
+print ("Chapter 7 teaches us about functions.") #extra
+d_message() #call on the function
