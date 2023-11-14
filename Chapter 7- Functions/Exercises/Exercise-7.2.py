fb54a96024ce7a7a71f614eabcc1e300a14c36d4
Alilymae <ortizaliah88@gmail.com>
2023-11-14 19:32:40 +0400

    Chapter 7 Ex 2

---
+++ Chapter 7- Functions/Exercises/Exercise-7.2.py
@@ -0,0 +1,7 @@
+#function w/ parameter (title)
+def favorite_book(title):
+    print (f"My favorite book among Rick Riordan's work is {title}")
+
+
+favorite_book("Blood of Olympus") #Blood of Olympus as argument
+print ("From the Heroes of Olympus series") #extra
