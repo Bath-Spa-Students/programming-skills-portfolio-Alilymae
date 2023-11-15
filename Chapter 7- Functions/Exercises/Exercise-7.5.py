2145cc65d0ee5d3481b24e849b658057262ca817
Alilymae <ortizaliah88@gmail.com>
2023-11-15 12:00:50 +0400

    Chapter 7 Ex 5

---
+++ Chapter 7- Functions/Exercises/Exercise-7.5.py
@@ -0,0 +1,9 @@
+#cities and countries function
+def d_city(city, country = "none"):
+    print (f"You should visit {city} in {country}!")
+
+
+#calling the citiess
+d_city("Busan", "South Korea")
+d_city("Schwangau", "Germany")
+d_city("Kyoto", "Japan") #diff. city & country
